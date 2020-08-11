6793bec2a5acb451e01ce9c7cdbf0a17da3dc340
Learner <pilikiwi@gmail.com>
2020-08-11 09:01:54 -0700

    4.2 Blog list, step2

---
+++ controllers/blogs.js
@@ -0,0 +1,48 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res)=>{
+    Blog.find({}).then(blogs=>{
+        res.json(blogs.map(blog => blog.toJSON()))
+    })
+})
+
+blogsRouter.get('/:id', (req, res, next) => {
+    Blog.findById(req.params.id)
+      .then(blog => {
+        if (blog) {
+          res.json(blog.toJSON())
+        } else {
+          res.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
+  
+  blogsRouter.post('/', (req, res, next) => {
+    const body = req.body
+  
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    })
+  
+    blog.save()
+      .then(savedBlog => {
+        res.json(savedBlog.toJSON())
+      })
+      .catch(error => next(error))
+  })
+  
+  blogsRouter.delete('/:id', (req, res, next) => {
+    Blog.findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+   
+  module.exports = blogsRouter
+  
